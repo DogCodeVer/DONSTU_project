a8b79a2ff86c19bdb007bc895a2731451404333e
rus12ka34 <ruska35@mail.ru>
2021-12-25 10:56:12 +0300

    Пиздец, нахуй, бляя

--- js/main.js
+++ js/main.js
@@ -15,6 +15,26 @@ $("#header-down-button-wrepper-id").on("click","a", function (event) {
 
 });
 
+$("#header-down-button-wrepper-idd").on("click","a", function (event) {
+  event.preventDefault();
+
+  let id  = $(this).attr('href');
+  let top = $(id).offset().top;
+
+  $('body,html').animate({scrollTop: top}, 800);
+
+});
+
+$("#header-down-button-wrepper-iddd").on("click","a", function (event) {
+  event.preventDefault();
+
+  let id  = $(this).attr('href');
+  let top = $(id).offset().top;
+
+  $('body,html').animate({scrollTop: top}, 800);
+
+});
+
 // scroll end
 	
 
